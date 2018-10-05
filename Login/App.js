84be971d8f105aa160f38953a442cc4fd9b8f069
Sevda Deniz <sevdadeniz3549@gmail.com>
2018-10-05 14:47:12 +0300

    ikinci

--- Login/App.js
+++ Login/App.js
@@ -39,6 +39,8 @@ export default class App extends Component {
         }
     }
 
+
+
     render() {
         return (
             <ImageBackground source={bgImage} style={styles.backgroundContainer}>
